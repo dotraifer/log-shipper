f05ffa7605b510ff74c00cfbb41fd190529eab41
Dotan Raifer <dotanraifer@gmail.com>
2023-04-29 02:12:43 +0300

    modify add Added - yaml problem to be handeld.

--- log-shipper-core/plugins/filter/Modify.cs
+++ log-shipper-core/plugins/filter/Modify.cs
@@ -16,12 +16,35 @@ namespace log_shipper.log_shipper_core.plugins.filter
         public override async Task Run(Event eventLog)
         {
             Log.Debug("start modify running");
+            eventLog = await AddFilter(eventLog);
             eventLog.LogData.Add("app", "test");
 
             foreach (var pipeline in this.NextPipelines)
             {
                 await pipeline.Run(eventLog);
             }
+        }
+
+        public async Task<Event> AddFilter(Event eventLog)
+        {
+            string key = string.Empty;
+            string value = string.Empty;
+            foreach(var field in this.PipelineConfiguration)
+            {
+                if (field.Key.ToString().ToLower().Equals("add"))
+                {
+                    string[] words = field.Value.ToString().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    // Split the string by space delimiter, and remove any empty substrings
+
+                    if (words.Length >= 2)
+                    {
+                        key = words[0];
+                        value = words[1];
+                        eventLog.LogData.Add(key, value);
+                    }
+                }
+            }
+            return eventLog;
         }
 
 
