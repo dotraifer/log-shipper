9f9a116df50bc54703f7a65867b36a605c8bc151
Dotan Raifer <dotanraifer@gmail.com>
2023-05-01 19:52:23 +0300

    had to go

--- log-shipper-core/plugins/parser/RegEx.cs
+++ log-shipper-core/plugins/parser/RegEx.cs
@@ -4,8 +4,11 @@ using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace log_shipper.log_shipper_core.plugins.parser
 {
@@ -16,12 +19,54 @@ namespace log_shipper.log_shipper_core.plugins.parser
         }
         public override async Task Run(Event eventLog)
         {
-            eventLog.LogData.Add("parser", "hey");
-            Log.Debug("start parser running");
+            try
+            {
+                await Console.Out.WriteLineAsync("pae");
+                eventLog = RegexParser(eventLog);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("RegexParserFailed");
+            }
             foreach (var pipeline in this.NextPipelines)
             {
                 await pipeline.Run(eventLog);
             }
+        }
+
+        private Event RegexParser(Event eventLog)
+        {
+            string reg;
+            try
+            {
+                reg = ((string)this.PipelineConfiguration["regex"]);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("regex label was not found in regex parser");
+                throw new Exception("to implement");
+            }
+            try
+            {
+                Regex rx = new Regex(reg);
+                string log = (string)eventLog.LogData["Log"];
+                MatchCollection matches = rx.Matches(log);
+                foreach (Match match in matches)
+                {
+                    GroupCollection groups = match.Groups;
+                    foreach (Group group in groups)
+                    {
+                        eventLog.LogData.Add(group.Name, group.Value);
+                    }
+                }
+
+            } 
+            catch (Exception ex) 
+            {
+                Log.Error("Regex failed");
+                throw new Exception();
+            }
+            return eventLog;
         }
     }
 }
